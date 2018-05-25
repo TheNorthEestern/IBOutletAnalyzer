c1b317b4cd11c06c7b0c99ad8e656d2b647ce40d
Kacy James <inquen@gmail.com>
2018-05-25 15:26:42 -0400

    <master> Did mild refactoring

--- outlet_analyzer.py
+++ outlet_analyzer.py
@@ -17,16 +17,17 @@ class Analyzer:
         }
         """
         output = Analyzer.call_grep(file_path)
-        class_name = re.compile("[\w-]+\.")
+        class_name_expr = re.compile("[\w-]+\.")
         var_name = re.compile("\*(.+?);")
         for line in iter(output.stdout.readline, b''):
             self.lines.append(str(line))
-        for l in self.lines:
-            if not class_name.findall(l)[0][:-1] in self.outlets:
-                self.outlets[class_name.findall(l)[0][:-1]] = []
-            if class_name.findall(l)[0][:-1] in self.outlets:
-                if var_name.findall(l):
-                    self.outlets[class_name.findall(l)[0][:-1]].append(var_name.findall(l)[0])
+        for line in self.lines:
+            class_name = class_name_expr.findall(line)[0][:-1] 
+            if not class_name in self.outlets:
+                self.outlets[class_name] = []
+            if class_name in self.outlets:
+                if var_name.findall(line):
+                    self.outlets[class_name].append(var_name.findall(line)[0])
 
     def print_methods(self):
         for class_name, variables_array in self.outlets.items():
@@ -42,7 +43,6 @@ class Analyzer:
 
     @staticmethod
     def call_grep(file_path):
-        # return call(["grep", "-r", "-i", "--include", "\*.h", "--include", "\*m", "IBOutlet", file_path], shell=True)
         return Popen("grep -r -i --include \*.h --include \*.m IBOutlet %s" % file_path, shell=True, stdout=PIPE)
 
 a = Analyzer(argv[1])
